5a3015c99bc50057f51e5a93d54ec63747b0c45f
Wis'kacha <daniel.viscarra@hotmail.com>
2024-10-09 01:11:58 -0400

    Plantilla clean

--- resources/views/pages/movimientos/editarMovimiento.blade.php
+++ resources/views/pages/movimientos/editarMovimiento.blade.php
@@ -699,7 +699,6 @@
 @endsection
 
 @section('js')
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js"></script>
@@ -707,7 +706,6 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
 
-    <script></script>
     <script>
         $(document).ready(function() {
             // Apply Select2 to the ID Empresa field
